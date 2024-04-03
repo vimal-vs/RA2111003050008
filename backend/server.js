19102f3784f3107848a9b854d8d862b9d6dfe213
Vimal Vs <vimalchan24@gmail.com>
2024-04-03 19:58:25 +0530

    auth-middleware created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const axios = require('axios');
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
 const PORT = 3001;
+const BASE_URL = process.env.API_URL;
+
 app.set('port', PORT);
 
+// auth-middleware
+const auth = async (req, res, next) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/test/auth`,
+            {
+                companyName: "goMart",
+                clientID: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                ownerName: "Vimal Sakkthi V S",
+                ownerEmail: "vv4861@srmist.edu.in",
+                rollNo: "RA2111003050008"
+            }
+        );
+        if (response.data.access_token) {
+            req.accessToken = response.data.access_token;
+            next();
+        }
+        else {
+            res.status(401).send({
+                message: "Invalid Auth Token"
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+}
 
 app.listen(PORT);
